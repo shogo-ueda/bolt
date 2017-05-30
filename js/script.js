19fa09d422653ae189c44b10cf34b758a9d1b764
shogo <uesan523@gmail.com>
2017-05-30 12:14:47 +0900

    '第一回レビュー'

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector('.bars');
+var menu = document.querySelector('.bars-open');
+
+btn.addEventListener('click', function() {
+  menu.classList.toggle('is-open');
+});
+
+
